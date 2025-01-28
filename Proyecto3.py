1380a7c8dd14aef1890ea133ae55228248e214c3
LeFattyBoi <pinneaple666@gmail.com>
2025-01-28 10:53:21 -0600

    Update Proyecto3.py

--- Proyecto3.py
+++ Proyecto3.py
@@ -1,4 +1,4 @@
-print ('---------------------------------------')
+print ('---------------------------------------') #AHHHHHHHHHHHHHHHHHHHHHHHHH
 print ('⤜    Inventario de almacenamiento    ⤛')
 print ('---------------------------------------')
 
