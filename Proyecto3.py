e87e1fbb435720e6a56095f2b5af804c3461301d
LeFattyBoi <pinneaple666@gmail.com>
2025-01-28 10:48:44 -0600

    Add files via upload

---
+++ Proyecto3.py
@@ -0,0 +1,35 @@
+print ('---------------------------------------')
+print ('⤜    Inventario de almacenamiento    ⤛')
+print ('---------------------------------------')
+
+usuario = input ('Favor de proprocionar su nombre: ') #Error aqui ¿"opcion no valida"?
+
+print ('Bienvenido: ',usuario)
+
+inventario = {} #producto y/o cantidades
+
+def agregar (inventario, nombre, cantidad):
+    if nombre in inventario:
+        inventario [nombre] += cantidad
+    else:
+        inventario [nombre] = cantidad
+
+def consultar (inventario, nombre):
+    return inventario.get (nombre, 'Producto no encontrado') 
+
+while True:
+    opcion = input ('Seleccione una opcion \nA) Agregar producto \nB) Consultar producto')
+    
+    if opcion == 'A': #Agregar producto
+        nombre = input ('Ingrese el nombre del producto: ')
+        cantidad = int (input ('Ingrese la cantidad del producto: '))
+        agregar (inventario, nombre, cantidad)
+        print ('Producto agregado correctamente al inventario')
+    
+    elif opcion == 'B': #Consultar producto
+        nombre = input ('Ingrese el nombre del producto: ')
+        print ('Consultar: ', consultar (inventario, nombre))
+    
+    else:
+        print ('Opcion no valida')
+        break
